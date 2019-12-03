774fdd6f6f58e3a0852c46d0f19ef1dbe5cdeea6
Alex Francia <55185967+SaltySausage@users.noreply.github.com>
2019-12-03 11:20:13 +0800

    Created Main Project File

---
+++ FinalProject.py
@@ -0,0 +1,41 @@
+def familyTree():
+
+
+	def load(filename):
+		f = open(str(filename) + ".ft","r")
+
+		lines = f.readlines()
+		print(lines)
+
+		print(lines[0])
+
+		print("end load")
+
+	def reset():
+		print("end reset")
+
+	
+
+load("TheFamilyName")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print('end program')
